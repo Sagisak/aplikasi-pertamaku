4a0f9e2c5f9ade4196cb79b8b721b7463f2bad9f
Sagisak <leonardo.tyoes@gmail.com>
2024-10-25 01:12:30 +0700

    update the vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/my_name/', // Set this to the path where your app is served
+  plugins: [vue()], // Keep the Vue plugin here
+  server: {
+    host: '0.0.0.0', // Ensure the server listens on all interfaces for Docker
+    port: 5173, // Make sure this matches your Docker configuration
+  },
 })
