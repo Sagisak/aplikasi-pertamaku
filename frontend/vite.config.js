04cbc6965e306d150b717eb9b9906fb44e8d1941
Sagisak <leonardo.tyoes@gmail.com>
2024-10-25 10:23:44 +0700

    psuh github

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/leonardo/',
   server: {
     host: '0.0.0.0', // Ensure the server listens on all interfaces for Docker
     port: 5173, // Make sure this matches your Docker configuration
