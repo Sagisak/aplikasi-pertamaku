f5185514b42e5bda0193299d580c9089f441d663
Sagisak <leonardo.tyoes@gmail.com>
2024-10-25 10:43:43 +0700

    changing the vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Ensure the server listens on all interfaces for Docker
     port: 5173, // Make sure this matches your Docker configuration
+    proxy: {
+      '/api': {
+        target: 'http://backend-container:3000', // Replace with the name of your backend Docker container
+        changeOrigin: true, // Needed for virtual hosted sites
+        secure: false, // If the backend uses HTTPS with a self-signed certificate, use this option
+      },
+    },
   },
-})
+});
