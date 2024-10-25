e9fc608cdeb684e0dee3870fc8cc1f1edc2c9f6a
Sagisak <leonardo.tyoes@gmail.com>
2024-10-25 14:09:40 +0700

    again?

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,8 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/leonardo/',
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://backend:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    hmr: {
-      host: '20.70.138.106',
-      protocol: 'ws',
-      port: 5173,
-    },
-  },
-});
+})
