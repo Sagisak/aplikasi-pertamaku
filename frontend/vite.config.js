7c9fe5b4b709a24ff29f09338382eef3cba38cd7
Sagisak <leonardo.tyoes@gmail.com>
2024-10-25 14:03:32 +0700

    !

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://backend:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    hmr: {
-      host: '20.70.138.106',
-      protocol: 'ws',
-      port: 5173,
-    },
-  },
-});
+  base: '/leonardo/',
+})
