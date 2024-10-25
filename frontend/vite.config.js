f838607f04476de3caacea4695a7883e7ba4d49d
Sagisak <leonardo.tyoes@gmail.com>
2024-10-25 13:36:51 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/leonardo/',
   server: {
     host: '0.0.0.0',
     port: 5173,
